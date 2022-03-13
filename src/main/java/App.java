5783bcff7e4a31fcf1814939800200cf6c0fa340
timothykirathe <timothy.kirathe@student.moringaschool.com>
2022-03-13 22:26:23 +0300

    Added invoice dao and added its respective routes

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,4 +1,5 @@
 import com.google.gson.Gson;
+import dao.Sql2oInvoiceDao;
 import dao.Sql2oServiceDao;
 import dao.Sql2oUserDao;
 import models.*;
@@ -15,6 +16,7 @@ public class App {
         Sql2oServiceDao sql2oServiceDao;
         DistanceCalculator distanceCalculator = new DistanceCalculator();
         Sql2oUserDao sql2oUserDao;
+        Sql2oInvoiceDao sql2oInvoiceDao;
         Connection conn;
         Gson gson = new Gson();
 
@@ -22,11 +24,12 @@ public class App {
         Sql2o sql2o = new Sql2o(connectionString, "lkyizybqkygkvk", "81e47c09943f1d05aabf6e71a744af70e738472850013f97193555842d8aaaa9");
         sql2oServiceDao = new Sql2oServiceDao(sql2o);
         sql2oUserDao = new Sql2oUserDao(sql2o);
+        sql2oInvoiceDao = new Sql2oInvoiceDao(sql2o);
         conn = sql2o.open();
 
         // POST METHODS
 
-        // Post Service to DB
+        // Post Service to DB.
         post("/services/new", "application/json", (req, res) -> {
             Service service = gson.fromJson(req.body(), Service.class);
             sql2oServiceDao.save(service);
@@ -35,7 +38,7 @@ public class App {
             return gson.toJson(postServiceResponse);
         });
 
-        // Post User to DB
+        // Post User to DB.
         post("/users/new", "application/json", (req, res) -> {
             System.out.println(req.body().toString());
             User user = gson.fromJson(req.body(), User.class);
@@ -47,7 +50,7 @@ public class App {
             return gson.toJson(postUserResponse);
         });
 
-        // User Logged In
+        // User Logged In.
         post("/users/login", "application/json", (req, res) -> {
             LoginUser loginUser = gson.fromJson(req.body(), LoginUser.class);
             User user = sql2oUserDao.findByEmail(loginUser.getEmail());
@@ -66,7 +69,7 @@ public class App {
             return gson.toJson(loginResponse);
         });
 
-        // Get distance between two points
+        // Get distance between two points.
         post("/calculate", "application/json", (req, res) -> {
             Calculate calculate = gson.fromJson(req.body(), Calculate.class);
             Service service = sql2oServiceDao.findById(calculate.getId());
@@ -80,10 +83,20 @@ public class App {
             return gson.toJson(response);
         });
 
+        // Post Invoice to DB.
+        post("/invoice/new", "application/json", (req, res)-> {
+            Invoice invoice = gson.fromJson(req.body(), Invoice.class);
+            sql2oInvoiceDao.save(invoice);
+            res.status(201);
+            PostResponse postResponse = new PostResponse(201, "Successfully Added", invoice);
+            return gson.toJson(postResponse);
+        });
+
+
 
         // GET METHODS
 
-        // Get all services
+        // Get all services.
         get("/services", "application/json", (req, res) -> {
             if (sql2oServiceDao.getAll().size() == 0) {
                 ErrorResponse errorResponse = new ErrorResponse(404, "Sorry, there are no services currently available.");
@@ -106,6 +119,40 @@ public class App {
             return gson.toJson(responseObject);
         });
 
+        // Get all invoices.
+        get("invoice/all", "application/json", (req, res) -> {
+            if (sql2oServiceDao.getAll().size() == 0) {
+                ErrorResponse errorResponse = new ErrorResponse(404, "Sorry, there are no services currently available.");
+                return gson.toJson(errorResponse);
+            }
+            ResponseList response = new ResponseList(200, "Success", sql2oServiceDao.getAll());
+            res.status(200);
+            return gson.toJson(response);
+        });
+
+        // Get all invoices for a specific user by user ID.
+        get("invoice/user/:id", "application/json", (req, res) -> {
+            int id = Integer.parseInt(req.params("id"));
+            if (sql2oInvoiceDao.findByUserId(id).size() == 0) {
+                ErrorResponse errorResponse = new ErrorResponse(404, "You have no invoices.");
+                return gson.toJson(errorResponse);
+            }
+            ResponseList responseList = new ResponseList(200, "Success", sql2oInvoiceDao.findByUserId(id));
+            res.status(200);
+            return gson.toJson(responseList);
+        });
+
+        // Get an invoice by its ID.
+        get ("invoice/:id", "application/json", (req, res) -> {
+            int id = Integer.parseInt(req.params("id"));
+            if (sql2oInvoiceDao.findById(id) == null) {
+                ErrorResponse errorResponse = new ErrorResponse(404, "Sorry, no such invoice exists.");
+                return gson.toJson(errorResponse);
+            }
+            ResponseObject responseObject = new ResponseObject(200, "Success", sql2oInvoiceDao.findById(id));
+            res.status(200);
+            return gson.toJson(responseObject);
+        });
 
         after((req, res) -> {
             res.type("application/json");
---
+++ src/main/java/dao/Sql2oInvoiceDao.java
@@ -0,0 +1,90 @@
+package dao;
+
+import models.Invoice;
+import models.Service;
+import org.sql2o.Connection;
+import org.sql2o.Sql2o;
+import org.sql2o.Sql2oException;
+
+import java.util.List;
+
+public class Sql2oInvoiceDao implements InvoiceDao {
+
+    private final Sql2o sql2o;
+
+    public Sql2oInvoiceDao(Sql2o sql2o)  {
+        this.sql2o = sql2o;
+    }
+
+    @Override
+    public void save(Invoice invoice) {
+        try(Connection conn = sql2o.open()){
+            String sql = "INSERT INTO invoice (userid, name, email, price, noofbedrooms, amount, latfrom, longfrom, latto, longto, date ) VALUES (:userId, :name, :email, :price, :noOfBedRooms, :amount, :latFrom, :longFrom, :latTo, :longTo, :date )";
+            int id = (int) conn.createQuery(sql).bind(invoice)
+                    .addParameter("userId", invoice.getUserId())
+                    .addParameter("name", invoice.getName())
+                    .addParameter("email", invoice.getEmail())
+                    .addParameter("price", invoice.getPrice())
+                    .addParameter("noOfBedRooms", invoice.getNoOfBedRooms())
+                    .addParameter("amount", invoice.getAmount())
+                    .addParameter("latFrom", invoice.getLatFrom())
+                    .addParameter("longFrom", invoice.getLongFrom())
+                    .addParameter("latTo", invoice.getLatTo())
+                    .addParameter("longTo", invoice.getLongTo())
+                    .addParameter("date", invoice.getDate())
+                    .executeUpdate().getKey();
+            invoice.setId(id);
+        }
+    }
+
+    @Override
+    public List<Invoice> getAll() {
+        try(Connection con = sql2o.open()) {
+            String sql = "SELECT * FROM invoice";
+            return con.createQuery(sql).executeAndFetch(Invoice.class);
+        }
+    }
+
+    @Override
+    public Invoice findById(int id) {
+        try (Connection conn = sql2o.open()) {
+            String sql = "SELECT * FROM invoice WHERE id = :id";
+            return conn.createQuery(sql).addParameter("id", id)
+                    .executeAndFetchFirst(Invoice.class);
+        }
+    }
+
+    @Override
+    public List<Invoice> findByUserId(int id) {
+        try (Connection conn = sql2o.open()) {
+            String sql = "SELECT * FROM invoice WHERE userid = :id";
+            return conn.createQuery(sql).addParameter("id", id)
+                    .executeAndFetch(Invoice.class);
+        } catch (Sql2oException ex) {
+            System.out.println(ex);
+        }
+        return null;
+    }
+
+    @Override
+    public void deleteById(int id) {
+        try(Connection conn = sql2o.open()) {
+            String sql = "DELETE FROM invoice WHERE id = :id";
+            conn.createQuery(sql).addParameter("id", id).executeUpdate();
+        } catch (Sql2oException ex) {
+            System.out.println(ex);
+        }
+    }
+
+    @Override
+    public void clearAll() {
+        String sql1 = "TRUNCATE TABLE invoice";
+        String sql2 = "ALTER SEQUENCE invoice_id_seq RESTART";
+        try(Connection conn = sql2o.open()) {
+            conn.createQuery(sql1).executeUpdate();
+            conn.createQuery(sql2).executeUpdate();
+        } catch (Sql2oException ex) {
+            System.out.println(ex);
+        }
+    }
+}
